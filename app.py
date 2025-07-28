2d0579ff784d40d2030bfb1ee8312efc7232c5d3
jufragarcaCUN <juan_garnicac@cun.edu.co>
2025-07-28 17:39:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def crear_app():
     Esta función encapsula toda la lógica de la aplicación, incluyendo rutas y funciones auxiliares,
     para que pueda ser importada y ejecutada por servidores WSGI (como Gunicorn en Render.com).
     """
-   app = Flask(__name__, static_folder='static', template_folder='templates')
+    app = Flask(__name__, static_folder='static', template_folder='templates')
 
 
     # Define la ruta base del directorio donde se encuentra este script.
